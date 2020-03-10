8fe77cdd442fe7e8ba0f24409d3957dff48f9793
unknown <thomas.berrens@me.com>
2020-03-10 09:22:45 +0100

    launch

---
+++ Assets/Scripts/gameplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameplay : MonoBehaviour
+{
+    public GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+         var mousePos = Input.mousePosition;
+         mousePos.z = 2.0f;       // we want 2m away from the camera position
+         var objectPos = Camera.current.ScreenToWorldPoint(mousePos);
+         Instantiate(cube, objectPos, Quaternion.identity);
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,6 @@
+Unity versie: 2017.1.0f3
+
+Gameplay:
+Je zit je cube constant bewegen over het platform heen de bedoeling is om op het juiste moment linkermuisknop
+te drukken als je het perfect doet word je cube niet gesliced als het niet perfect gedaan word sliced je cube en word je
+platform kleiner als je bruut mist of je platform is te klein is het gameover.
